5f0192a477479a3564a5ea7c029a1c91ec864618
Mari Caires <mari_tc14@hotmail.com>
2025-09-30 16:31:46 -0300

    Adding classes generate from UML

---
+++ Code/level.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from Interface2 import Interface2
+
+
+class Level(Interface2):
+    def __init__(self):
+        self.windom = None
+        self.name = None
+        self.entity_list = None
+
+    def run(self, ):
+        pass
