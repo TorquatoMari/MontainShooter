bfb00b3378b3ca5077fe8818cd8d1685b9f5d029
Mari Caires <mari_tc14@hotmail.com>
2025-09-30 16:56:06 -0300

    Adding classes generate from UML

--- Code/game.py
+++ Code/game.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame
 
 class Game:
     def __init__(self):
         self.window = None
 
-    def run(self, ):
-        pass
+
+    def run(self,):
+        print('Setup Start')
+        pygame.init()
+        window = pygame.display.set_mode(size=(600, 480))
+
+        while True:
+             # Check for all events
+             for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                  pygame.quit()  # close windom
+                  quit()  # end pygame
+
+
+
