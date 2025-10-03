a78fd4409e3906580d1f820ac2cfa0ce7333af13
Mari Caires <mari_tc14@hotmail.com>
2025-10-03 11:51:39 -0300

    Merge remote-tracking branch 'origin/master'

---
+++ Code/const.py
@@ -0,0 +1,16 @@
+from pygame.examples.grid import WINDOW_WIDTH
+#C
+COLOR_ORANGE= (255,128,0)
+COLOR_WHITE= (255,255,255)
+
+#M
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW_GAME 2P - COOPERATIVE',
+               'NEW_GAME 2) - COMPETITIVE',
+               'SCORE',
+               'EXIT')
+
+
+# W
+WIN_WIDTH=576
+WIN_HEIGHT=324
--- Code/game.py
+++ Code/game.py
@@ -2,26 +2,19 @@
 # -*- coding: utf-8 -*-
 import pygame
 
+from Code.const import WIN_WIDTH, WIN_HEIGHT
 from Code.menu import Menu
 
 
 class Game:
     def __init__(self):
-        self.window = None
         pygame.init()
-        window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH,WIN_HEIGHT))
 
-    def run(self,):
-        print('Setup Start')
+    def run(self):
         while True:
             menu = Menu(self.window)
             menu.run()
             pass
-             # Check for all events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                  pygame.quit()  # close windom
-                  quit()  # end pygame
-
 
 
